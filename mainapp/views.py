7ca34be6338591e0791b6b45a88bfdd7bfa3177e
impreza555 <alex.erjomin@outlook.com>
2021-11-23 22:34:50 +0300

    Сделал домашнее задание 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 from django.shortcuts import render
 
 
@@ -8,9 +11,47 @@ def main(request):
     return render(request, 'mainapp/index.html', context)
 
 
+module_dir = os.path.dirname(__file__)
+file_path = os.path.join(module_dir, 'fixtures/links_menu.json')
+links_menu = json.load(open(file_path, encoding='utf-8'))
+
+
 def products(request):
     context = {
         'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_home(request):
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_office(request):
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_modern(request):
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_classic(request):
+    context = {
+        'title': 'Продукты',
+        'links_menu': links_menu
     }
     return render(request, 'mainapp/products.html', context)
 
