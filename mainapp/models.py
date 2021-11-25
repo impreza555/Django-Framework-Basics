b7d58412983b7023c6cb04d9c7d21060a809e23b
impreza555 <alex.erjomin@outlook.com>
2021-11-25 18:12:22 +0300

    Создал модели "Категории продуктов" и "Продукты"

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория товара'
+        verbose_name_plural = 'Категории товаров'
+        ordering = ('-id',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=128, verbose_name='Название')
+    image = models.ImageField(upload_to='products_images', blank=True, null=True, verbose_name='Изображение')
+    short_desc = models.CharField(max_length=128, verbose_name='Краткое описание')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('-id',)
