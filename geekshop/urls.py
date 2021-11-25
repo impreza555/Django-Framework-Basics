ea4d83536b74c235041c798338a4f4d4f1f55b05
impreza555 <alex.erjomin@outlook.com>
2021-11-25 22:11:27 +0300

    Добавил настройки в urls и setting

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from mainapp import views as mainapp
 
 urlpatterns = [
     path('', mainapp.main, name='index'),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('products/classic/', mainapp.products_classic, name='products_classic'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
