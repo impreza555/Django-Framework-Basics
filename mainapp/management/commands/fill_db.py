86939d34f6c24e8f486324676c64cf7dc75d3d99
impreza555 <alex.erjomin@outlook.com>
2021-12-01 02:53:52 +0300

    Переделал файл fill_db.py, создал файлы json для заполнения БД, добавил новое приложение.

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,36 +1,30 @@
 from django.core.management.base import BaseCommand
 from mainapp.models import ProductCategory, Product
-from django.contrib.auth.models import User
-import json, os
+from authapp.models import ShopUser
+import os, json
+
 
 JSON_PATH = 'mainapp/json'
 
 
 def load_from_json(file_name):
-    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf8') as infile:
         return json.load(infile)
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
         categories = load_from_json('categories')
-
         ProductCategory.objects.all().delete()
         for category in categories:
-            new_category = ProductCategory(**category)
-            new_category.save()
-
+            ProductCategory.objects.create(**category)
         products = load_from_json('products')
-
         Product.objects.all().delete()
         for product in products:
             category_name = product["category"]
-            # Получаем категорию по имени
-            _category = ProductCategory.objects.get(name=category_name)
-            # Заменяем название категории объектом
-            product['category'] = _category
-            new_product = Product(**product)
-            new_product.save()
+            category_item = ProductCategory.objects.get(name=category_name)
+            product["category"] = category_item
+            Product.objects.create(**product)
 
-        # Создаем суперпользователя при помощи менеджера модели
-        super_user = User.objects.create_superuser('django', 'django@geekshop.local', 'geekbrains')
+        ShopUser.objects.create_superuser('django', password='geekbrains', age=18)
