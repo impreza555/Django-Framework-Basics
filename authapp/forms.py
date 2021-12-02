28bdf44b882cbab39b999dadb7a3cd4b716dbc2d
impreza555 <alex.erjomin@outlook.com>
2021-12-02 17:33:02 +0300

    Убрал ненужную строчку в forms.py

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
-#from django.contrib.auth.forms import PasswordChangeForm
 from django import forms
 
 from .models import ShopUser
