6944061a85341f10f288d6f1a76102f5a7189b9b
impreza555 <alex.erjomin@outlook.com>
2021-12-03 17:29:58 +0300

    Добавил шаблон products_list, добавил соотв. контроллер и запись в урлах, переделал главное меню и меню категорий.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,6 +3,6 @@ from mainapp import views as mainapp
 
 app_name = 'mainapp'
 urlpatterns = [
-    path('', mainapp.products, name='products'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('', mainapp.products, name='index'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.models import ProductCategory, Product
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -11,9 +12,29 @@ def index(request):
 
 
 def products(request, pk=None):
+    title = 'Продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+        return render(request, 'mainapp/products_list.html', context)
+
+    same_products = Product.objects.all()[3:5]
     context = {
-        'title': 'Продукты',
-        'links_menu': ProductCategory.objects.all(),
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products,
     }
     return render(request, 'mainapp/products.html', context)
 
