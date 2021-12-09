38168074748783bd85d0672784f549e1555b4413
impreza555 <alex.erjomin@outlook.com>
2021-12-09 19:15:54 +0300

    Написал методы read и delete модели корзины, добавил шаблоны корзины.

--- basketapp/views.py
+++ basketapp/views.py
@@ -4,8 +4,13 @@ from mainapp.models import Product
 
 
 def basket(request):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+    title = 'корзина'
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+    context = {
+        'title': title,
+        'basket_items': basket_items,
+    }
+    return render(request, 'basketapp/basket.html', context)
 
 
 def basket_add(request, pk):
@@ -19,6 +24,8 @@ def basket_add(request, pk):
 
 
 def basket_remove(request, pk):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,6 +11,16 @@ def get_basket(user):
     return []
 
 
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
+
 def index(request):
     context = {
         'title': 'Главная',
@@ -23,10 +33,12 @@ def index(request):
 def products(request, pk=None):
     title = 'Продукты'
     links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
             category = {'name': 'все', 'pk': 0}
+
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
@@ -36,12 +48,12 @@ def products(request, pk=None):
             'links_menu': links_menu,
             'category': category,
             'products': products,
-            'basket': get_basket(request.user),
+            'basket': basket,
         }
         return render(request, 'mainapp/products_list.html', context)
 
-    hot_product = random.sample(list(Product.objects.all().order_by()), 3)[0]
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     context = {
         'title': title,
         'links_menu': links_menu,
