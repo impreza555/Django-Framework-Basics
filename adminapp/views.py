cbac7e90d54082a1497b7c4fdab7a0b320b73fc0
impreza555 <alex.erjomin@outlook.com>
2021-12-14 20:42:50 +0300

    Добавил в админку методы работы с пользователями, добавил соответствующие шаблоны, ограничил доступ неуполномоченным пользователям при помощи декораторов.

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
+from django.contrib.auth.decorators import user_passes_test
+from django.urls import reverse
 from authapp.models import ShopUser
-from django.shortcuts import get_object_or_404, render
+from authapp.forms import ShopUserRegisterForm
+from adminapp.forms import ShopUserAdminEditForm
 from mainapp.models import Product, ProductCategory
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
@@ -13,18 +18,49 @@ def users(request):
     return render(request, 'adminapp/users.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+    if user_form.is_valid():
+        user_form.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+    context = {'title': title, 'update_form': user_form}
+    return render(request, 'adminapp/user_update.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+    context = {'title': title, 'update_form': edit_form}
+    return render(request, 'adminapp/user_update.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+    user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        # user.delete()
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+    context = {'title': title, 'user_to_delete': user}
+    return render(request, 'adminapp/user_delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = 'админка/категории'
     categories_list = ProductCategory.objects.all()
@@ -35,18 +71,22 @@ def categories(request):
     return render(request, 'adminapp/categories.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_update(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_delete(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
     title = 'админка/продукт'
     category = get_object_or_404(ProductCategory, pk=pk)
@@ -59,17 +99,21 @@ def products(request, pk):
     return render(request, 'adminapp/products.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
     pass
